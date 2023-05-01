1a2215379e578a087be51b899c9169211569d292
Kinyua <alfkinyua@gmail.com>
2023-05-01 18:19:37 +0300

    Display stock items

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,3 +1,18 @@
+import storeItems from './../data/item.json';
+import { Row, Col } from 'react-bootstrap';
+import { StoreItem } from './../components/StoreItem';
+
 export function Store() {
-  return <div>Store</div>;
+  return (
+    <>
+      <h2>Store</h2>
+      <Row md={2} xs={1} lg={3} className="g-3">
+        {storeItems.map((item) => (
+          <Col key={item.id}>
+            <StoreItem {...item} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
 }
